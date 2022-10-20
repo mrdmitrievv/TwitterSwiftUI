c5fb3e7575b8e50d0024c964ef9888e0a775026d
Artem <mrdmitrievv@yandex.ru>
2022-10-20 12:14:58 +0300

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Артём Дмитриев on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
