1854bbbb6d541ddadc06026ac097051d53cafac2
Artem <mrdmitrievv@yandex.ru>
2022-11-07 17:44:28 +0300

    Upload data to Firestore and Storage

--- TwitterSwiftUI/ViewModels/AuthViewModel.swift
+++ TwitterSwiftUI/ViewModels/AuthViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseStorage
 
 class AuthViewModel: ObservableObject {
     
@@ -15,16 +16,43 @@ class AuthViewModel: ObservableObject {
     }
     
     func registerUser(email: String, fullname: String, username: String, password: String, userImage: UIImage) {
+                        
+        guard let imageData = userImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        storageRef.putData(imageData) { _, error in
             if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
+                print("DEBUG: Failed to upload immage \(error.localizedDescription)")
                 return
             }
             
-            print("Successfully registered..")
+            storageRef.downloadURL { url, _ in
+                guard let imageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else { return }
+                    
+                    let data = [
+                        "email": email,
+                        "fullname": fullname,
+                        "username": username,
+                        "userPhotoURL": imageUrl,
+                        "uid": user.uid
+                    ]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG: SUCCESS")
+                    }                    
+                    
+                }
+            }
+            
         }
-        
     }
-    
 }
