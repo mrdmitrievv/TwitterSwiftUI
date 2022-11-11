0af329ed7125e48a02dfcbcd96ff963c5ec94ab1
Artem <mrdmitrievv@yandex.ru>
2022-11-11 18:52:14 +0300

    Add api call to check is user isFollowed or not

--- TwitterSwiftUI/ViewModels/ProfileViewModel.swift
+++ TwitterSwiftUI/ViewModels/ProfileViewModel.swift
@@ -15,6 +15,7 @@ class ProfileViewModel: ObservableObject {
     
     init(user: User) {
         self.user = user
+        checkIfUserIsFollowed()
     }
     
     func follow() {
@@ -39,6 +40,16 @@ class ProfileViewModel: ObservableObject {
             followersRef.document(currentUid).delete { _ in
                 self.isFollowed = false
             }
+        }
+    }
+    
+    func checkIfUserIsFollowed() {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+        
+        followingRef.document(user.id).getDocument { snapshot, _ in
+            guard let isFollowed = snapshot?.exists else { return }
+            self.isFollowed = isFollowed
         }
     }
 }
