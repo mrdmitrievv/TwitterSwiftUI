052a807dc35bc9bc2e7a9c4285e6390e85cdb898
Artem <mrdmitrievv@yandex.ru>
2022-12-15 19:48:44 +0300

    Minor fixes

--- TwitterSwiftUI/View/Profile/FilterButtonView.swift
+++ TwitterSwiftUI/View/Profile/FilterButtonView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 enum TweetFilterOptions: Int, CaseIterable {
     case tweets
-//    case replies
     case likes
     
     var title: String {
         switch self {
         case .tweets: return "Tweets"
-//        case .replies: return "Tweets & Replies"
         case .likes: return "Likes"
         }
     }
